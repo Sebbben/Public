618f2e49d2f2d9956ec7f350f91f11060b161eb9
Sebbben <42118203+Sebbben@users.noreply.github.com>
2022-10-01 14:53:09 +0200

    Delete CryptoTrack directory

--- CryptoTrack/app/script.js
+++
@@ -1,7 +0,0 @@
-chrome.storage.local.set({ "phasersTo": "awesome" }, function(){
-    //  Data's been saved boys and girls, go on home
-});
-
-chrome.storage.local.get(/* String or Array */["phasersTo"], function(items){
-    console.log(items)
-});
